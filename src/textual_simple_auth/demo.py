dafece633e17ac39917123545d33dad2b5899c4f
David Fokkema <davidfokkema@icloud.com>
2024-06-13 10:41:22 +0200

    WIP: workable method for showing login screen first

--- src/textual_simple_auth/demo.py
+++ src/textual_simple_auth/demo.py
@@ -1,5 +1,28 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Label
+import time
+
+from textual.app import App, ComposeResult, on
+from textual.screen import Screen
+from textual.widgets import Button, Label
+
+
+class MyScreen(Screen):
+
+    def compose(self) -> ComposeResult:
+        yield Button("Login", id="login")
+        yield Button("Cancel", id="cancel")
+
+    @on(Button.Pressed)
+    def login(self, event: Button.Pressed) -> None:
+        if event.button.id == "login":
+            self.app.auth_successful = True
+        self.app.exit()
+
+
+class Login(App):
+    auth_successful = False
+
+    def on_mount(self) -> None:
+        self.push_screen(MyScreen())
 
 
 class SimpleAuthDemo(App):
@@ -7,6 +30,12 @@ class SimpleAuthDemo(App):
 
     def compose(self) -> ComposeResult:
         yield Label("This should only be accessible after login.")
+
+    def run(self) -> None:
+        login_app = Login()
+        login_app.run()
+        if login_app.auth_successful is True:
+            super().run()
 
 
 if __name__ == "__main__":
